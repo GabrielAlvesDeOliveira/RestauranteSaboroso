cd474ab35f39281e2f77284cd1b8326d611cd001
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-03 17:44:00 -0300

    first commit

---
+++ saboroso/inc/menus.js
@@ -0,0 +1,21 @@
+let conn = require('./db')
+
+module.exports = {
+
+    getMenus(){
+
+        return new Promise((s,f)=>{
+
+            conn.query(`SELECT * FROM tb_menus ORDER BY title`, (err, result)=>{
+            if(err){
+                f(err)
+            }
+            
+            s(result)
+
+            })
+
+        })
+
+    }
+}
---
+++ saboroso/routes/index.js
@@ -0,0 +1,57 @@
+var conn = require('../inc/db')
+var menus = require('../inc/menus')
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  menus.getMenus().then(results =>{
+
+    res.render('index', { 
+      title: 'Restaurante Saboroso', 
+      menus: results 
+    });
+
+  })
+  
+
+});
+
+router.get('/contacts',function(req,res,next){
+
+  res.render('contacts', {title: 'Contato - Restaurante saboroso',
+  background: 'images/img_bg_3.jpg',
+  h1: 'Diga um oi!'
+})
+
+})
+
+router.get('/menus',function(req,res,next){
+
+  menus.getMenus().then(results=>{
+
+    res.render('menus', {title: 'menus - Restaurante saboroso',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu',
+    menus: results})
+  })
+
+})
+
+router.get('/reservations',function(req,res,next){
+
+  res.render('reservations', {title: 'reservas - Restaurante saboroso',
+  background: 'images/img_bg_2.jpg',
+  h1: 'Reserve uma mesa'})
+
+})
+
+router.get('/services',function(req,res,next){
+
+  res.render('services', {title: 'servicos - Restaurante saboroso',
+  background: 'images/img_bg_1.jpg',
+  h1: 'E um prazer poder servir!'})
+
+})
+module.exports = router;
