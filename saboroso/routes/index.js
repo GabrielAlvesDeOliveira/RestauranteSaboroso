deaaca41aaa01010380524025a251e5484aac11a
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-06 17:25:50 -0300

    ok

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,6 +1,9 @@
 var conn = require('../inc/db')
 var menus = require('../inc/menus')
 var express = require('express');
+var reservations = require('../inc/reservation')
+var contacts = require('../inc/contacts');
+const { save } = require('../inc/reservation');
 var router = express.Router();
 
 /* GET home page. */
@@ -10,7 +13,8 @@ router.get('/', function(req, res, next) {
 
     res.render('index', { 
       title: 'Restaurante Saboroso', 
-      menus: results 
+      menus: results,
+      isHome: true
     });
 
   })
@@ -20,10 +24,31 @@ router.get('/', function(req, res, next) {
 
 router.get('/contacts',function(req,res,next){
 
-  res.render('contacts', {title: 'Contato - Restaurante saboroso',
-  background: 'images/img_bg_3.jpg',
-  h1: 'Diga um oi!'
+  contacts.render(req,res)
+
 })
+
+router.post('/contacts',function(req,res,next){
+
+  if(!req.body.name){
+    contacts.render(req,res,'digite o nome')
+  }else if(!req.body.email){contacts.render(req,res,'digite o email')}
+  else if(!req.body.message){contacts.render(req,res,'digite a mensagem')}
+  else{
+
+    contacts.save(req.body).then(results =>{
+
+      req.body = {}
+
+      contacts.render(req,res,null, 'Contato enviado com sucesso!')
+      
+    }).catch(err=>{
+
+      contacts.render(req,res,err.message)
+
+    })
+
+  }
 
 })
 
@@ -41,9 +66,37 @@ router.get('/menus',function(req,res,next){
 
 router.get('/reservations',function(req,res,next){
 
-  res.render('reservations', {title: 'reservas - Restaurante saboroso',
-  background: 'images/img_bg_2.jpg',
-  h1: 'Reserve uma mesa'})
+  reservations.render(req,res)
+
+})
+
+router.post('/reservations',function(req,res,next){
+
+  if(!req.body.name){
+    reservations.render(req,res, "Digite o nome")
+  }else if(!req.body.email){
+    reservations.render(req,res, "digite um email")
+  }else if(!req.body.people){
+    reservations.render(req,res, "selecione o numero de pessoas")
+  }else if(!req.body.date){
+    reservations.render(req,res, "selecione o dia")
+  }else if(!req.body.time){
+    reservations.render(req,res, "selecione o horario")
+  }else{
+
+    reservations.save(req.body).then(results =>{
+
+      req.body = {}
+
+      reservations.render(req,res, null, "Reserva realizada")
+
+    }).catch(err=>{
+
+      reservations.render(req,res,err.message)
+
+    })
+
+  }
 
 })
 
