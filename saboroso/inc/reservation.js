6a5dd4912a25e3e69e8b8f67ead0a68ed234b327
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-08 14:08:40 -0300

    doing Reservations CRUD

--- saboroso/inc/reservation.js
+++ saboroso/inc/reservation.js
@@ -17,17 +17,42 @@ module.exports = {
 
         return new Promise((res,rej)=>{
 
-            let date = fields.date.split('/')
+            if(fields.date.indexOf('/') > -1){
+                
+                let date = fields.date.split('/')
+    
+                fields.date = `${date[2]}-${date[1]}-${date[0]}`
+            }
 
-            fields.date = `${date[2]}-${date[1]}-${date[0]}`
 
-            conn.query(`INSERT INTO tb_reservations (name,email,people,date,time) VALUES (?,?,?,?,?)`,[
+            let query, params = [
                 fields.name,
                 fields.email,
                 fields.people,
                 fields.date,
                 fields.time
-            ], (err, results)=>{
+            ]
+
+            if(parseInt(fields.id) > 0){
+
+                query = `
+                    UPDATE tb_reservations
+                    SET name = ?,
+                    email = ?,
+                    people = ?,
+                    date = ?,
+                    time = ?
+                    WHERE id = ?
+                `
+                params.push(fields.id)
+
+            }else{
+
+                query = `INSERT INTO tb_reservations (name,email,people,date,time) VALUES (?,?,?,?,?)`
+
+            }
+
+            conn.query(query,params, (err, results)=>{
     
                 if(err){
                     rej(err)
@@ -39,6 +64,42 @@ module.exports = {
 
         })
 
+    },
+
+    getReservations(){
+
+        return new Promise((s,f)=>{
+
+            conn.query(`SELECT * FROM tb_reservations ORDER BY date DESC`, (err, result)=>{
+            if(err){
+                f(err)
+            }
+            
+            s(result)
+
+            })
+
+        })
+
+    },
+
+    delete(id){
+
+        return new Promise((resolve, reject)=>{
+
+            conn.query(`DELETE FROM tb_reservations WHERE id = ?`, [
+                id
+            ], (err,results)=>{
+
+                if(err){
+                    reject(err)
+                }else{
+                    resolve(results)
+                }
+
+            })
+
+        })
     }
 
 }
