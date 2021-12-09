6d5076e66c34f5b80e2cfa52727cb7a3351e52ed
GabrielAlvesdeOliveira <alvesg2003@gmail.com>
2021-12-09 17:10:42 -0300

    making graphics

--- saboroso/inc/reservation.js
+++ saboroso/inc/reservation.js
@@ -1,5 +1,6 @@
 const conn = require('./db')
 const Pagination = require('./Pagination')
+var moment = require('moment')
 
 module.exports = {
 
@@ -120,6 +121,50 @@ module.exports = {
             })
 
         })
+    },
+
+    chart(req){
+
+        return new Promise((resolve,reject)=>{
+
+            conn.query(`
+            SELECT 
+            CONCAT(YEAR(date), '-', MONTH(date)) AS date,
+             COUNT(*) AS total, 
+             SUM(people) / COUNT(*) AS avg_people 
+             FROM tb_reservations 
+             WHERE date BETWEEN ? AND  ?
+             GROUP BY CONCAT(YEAR(date), '-', MONTH(date))
+             ORDER BY date DESC
+            `,[
+                req.query.start,
+                req.query.end
+            ],(err, results)=>{
+
+                if(err){
+                    reject(err)
+                }else{
+                    let months = []
+                    let values = []
+
+                    results.forEach(row=>{
+
+                        months.push(moment(row.date).format('MMM YYYY'))
+                        values.push(row.total)
+
+                    })
+
+                    resolve({
+                        months,
+                        values
+                    })
+
+                }
+
+            })
+
+        })
+
     }
 
 }
